04fea78b137746ccededa0b2b52e8c056e063d19
karinadesposito <110677129+karinadesposito@users.noreply.github.com>
2023-08-17 00:52:35 -0300

    actualización de css

--- frontend/src/Card.jsx
+++ frontend/src/Card.jsx
@@ -3,8 +3,8 @@ function Card({track}) {
   return (
   <div className="card-container">
     <card className="track-card" id={track.id}>
-        <h1>{track.artist}</h1>
-        <h2>{track.title}</h2>
+    <h1 class="hoover">{track.artist}</h1>
+        <h2 class="hoover">{track.title}</h2>
         <p>{track.duration}</p>
     </card>
  
